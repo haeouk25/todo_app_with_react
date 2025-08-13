f1ec1eff19df1cb2f92b02ad6384b6c459967f21
haeouk <haeouk25@gmail.com>
2025-08-13 15:12:16 +0900

    backend

--- backend/src/main/java/haeouk25/backend/dto/TodoUpdateDto.java
+++ backend/src/main/java/haeouk25/backend/dto/TodoUpdateDto.java
@@ -1,4 +1,20 @@
 package haeouk25.backend.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoUpdateDto {
+    private String title;
+    private String description;
+
+    @NotNull(message = "완료 상태를 지정해주세요.")
+    private Boolean completed;
 }
